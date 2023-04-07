ce0697c85be458e714f6826ec39e9a2346f1a395
oshadaera68 <mindartlk010@gmail.com>
2023-04-07 21:57:13 +0530

    updated content

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,2 +1,15 @@
+import java.sql.SQLException;
+import java.util.Date;
+
 public class AppInitializer {
+    public static void main(String[] args) {
+        CrudManager crudManager = new CrudManager();
+        Account account = new Account("A-5", "jagath", 50000);
+        Payment payment = new Payment("P-5", new Date(), 4500);
+        try {
+            System.out.println(crudManager.saveData(account, payment));
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/CrudManager.java
+++ src/CrudManager.java
@@ -5,24 +5,40 @@ import java.sql.SQLException;
 public class CrudManager {
     public boolean saveData(Account account, Payment payment) throws SQLException, ClassNotFoundException {
         Connection connection = DbConnection.getInstance().getConnection();
-        PreparedStatement preparedStatement1 = connection.prepareStatement(
-                "INSERT INTO account VALUES(?,?,?)"
-        );
-        preparedStatement1.setString(1, account.getAccountId());
-        preparedStatement1.setString(2, account.getName());
-        preparedStatement1.setDouble(3, account.getAmount());
-
-        boolean isSaved = preparedStatement1.executeUpdate() > 0;
-        if (isSaved) {
-            PreparedStatement preparedStatement2 = connection.prepareStatement(
-                    "INSERT INTO payment VALUES(?,?,?,?)"
+        connection.setAutoCommit(false);
+        try {
+            PreparedStatement preparedStatement1 = connection.prepareStatement(
+                    "INSERT INTO account VALUES(?,?,?)"
             );
-            preparedStatement2.setString(1, payment.getPayId());
-            preparedStatement2.setObject(2, payment.getPayDate());
-            preparedStatement2.setDouble(3, payment.getPayment());
-            preparedStatement2.setString(4, account.getAccountId());
-            return preparedStatement2.executeUpdate() > 0;
+            preparedStatement1.setString(1, account.getAccountId());
+            preparedStatement1.setString(2, account.getName());
+            preparedStatement1.setDouble(3, account.getAmount());
+
+            boolean isSaved = preparedStatement1.executeUpdate() > 0;
+            if (isSaved) {
+                if (true){
+                    connection.rollback();
+                    return false;
+                }
+                PreparedStatement preparedStatement2 = connection.prepareStatement(
+                        "INSERT INTO payment VALUES(?,?,?,?)"
+                );
+                preparedStatement2.setString(1, payment.getPayId());
+                preparedStatement2.setObject(2, payment.getPayDate());
+                preparedStatement2.setDouble(3, payment.getPayment());
+                preparedStatement2.setString(4, account.getAccountId());
+                if (preparedStatement2.executeUpdate() > 0) {
+                    connection.commit();
+                    return true;
+                }
+                connection.rollback();
+            }
+            return false;
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        } finally {
+            connection.setAutoCommit(true);
         }
-        return false;
     }
 }
